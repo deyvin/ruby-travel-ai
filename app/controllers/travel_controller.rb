0bb260332624f553f7bc91ee1c9d3319b2850f0a
Deyvid Nascimento <deyvin@gmail.com>
2023-04-20 19:15:07 -0300

    refactor(travel): Move action to factory

--- app/controllers/travel_controller.rb
+++ app/controllers/travel_controller.rb
@@ -4,11 +4,6 @@ require './lib/travel'
 
 class TravelController
   def self.plan(params)
-    Travel
-      .new
-      .with_dates(params[:start_date], params[:end_date])
-      .from(params[:city_origin])
-      .to(params[:city_destination])
-      .plan!(params[:type])
+    PlanTravelFactory.new(params).perform!
   end
 end
