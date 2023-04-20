c8e898878871550711e56482e50005b13ea9bd8d
Deyvid Nascimento <deyvin@gmail.com>
2023-04-20 18:12:21 -0300

    feat(app): App routes

--- app.rb
+++ app.rb
@@ -1,5 +1,14 @@
 require 'sinatra'
+require 'byebug'
+require './lib/travel'
 
-get '/' do
-  'Hello'
+post '/plan' do
+  result = Travel
+    .new
+    .with_dates(params[:start_date], params[:end_date])
+    .from(params[:city_origin])
+    .to(params[:city_destination])
+    .plan!(params[:type])
+    
+  {result: result }.to_json
 end
